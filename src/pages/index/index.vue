881ae0e585ff88036052309f6412d0f7db8a3e71
XiaoYu-insist <2320055256@qq.com>
2025-02-10 17:02:43 +0800

    2.10

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,9 +3,10 @@
 </script>
 
 <template>
-  <view class="index">index</view>
+  <view class="index">index11</view>
+  <uni-collapse-item title="默认开启" :open="true">
+    <text>折叠内容</text>
+  </uni-collapse-item>
 </template>
 
-<style lang="scss">
-//
-</style>
+<style lang="scss"></style>
